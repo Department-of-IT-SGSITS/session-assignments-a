c6d6e14723b5cd79d9cfe3da98edf3957b0a770f
Anunay <anunay.j1@gmail.com>
2025-08-24 00:00:00 +0530

    why did this happen: {"success":false,"message":"A Firestore error occur

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,13 +1,8 @@
 import admin from 'firebase-admin';
 
-const firebaseConfig = {
-    projectId: "studio-2177517838-2b6e7",
-};
-
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.applicationDefault(),
-    databaseURL: `https://${firebaseConfig.projectId}.firebaseio.com`
   });
 }
 
